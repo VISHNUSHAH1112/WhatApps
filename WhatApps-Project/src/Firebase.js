a37087b0552fc734741d1fd98dc25a42eedd8ae3
VIshnu Shah <shahvishnu1112@gmail.com>
2025-09-17 20:14:11 +0530

    start

---
+++ WhatApps-Project/src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw47pEjA2kc3JWttdLAlDG7T7UzUVBtnk",
+  authDomain: "whatapps-eaeb2.firebaseapp.com",
+  projectId: "whatapps-eaeb2",
+  storageBucket: "whatapps-eaeb2.firebasestorage.app",
+  messagingSenderId: "433562592369",
+  appId: "1:433562592369:web:cd6190929d6e8f9950e7d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
